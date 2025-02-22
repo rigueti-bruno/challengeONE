db35133689ace3121f54457808e59f454214570b
rigueti-bruno <bruno.rigueti@outlook.com>
2025-02-22 11:33:38 -0300

    Código JavaScript para a entrega

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Lista de nomes a serem sorteados
+let amigos = [];
+
+// Função que adiciona um nome a lista de amigos
+function adicionarAmigo() {
+    document.getElementById('resultado').innerHTML = "";
+    let amigo = document.getElementById('amigo').value.trim();
+    if (amigo != "" && !amigos.includes(amigo)) {
+        amigos.push(amigo);
+        document.getElementById('amigo').value = "";
+        document.getElementById('listaAmigos').innerHTML = amigos.map(amigos => `<li>${amigos}</li>`).join("");
+    } else {
+        alert("Por favor, insira um nome válido.");
+        document.getElementById('amigo').value = "";
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length >= 2) {
+        let range = amigos.length;
+        let sorteado = Math.floor(Math.random() * range);
+        document.getElementById('resultado').innerHTML = `O amigo secreto sorteado é: ${amigos[sorteado]}`;
+        document.getElementById('listaAmigos').innerHTML = "";
+        amigos = [];
+    } else {
+        alert("Adicione pelo menos 2 amigos para sortear!");
+    }
+}
